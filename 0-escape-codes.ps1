552ca9863c8c57f73a61921b7825f13e1c2e66e8
Shaun Lawrie <shaun.r.lawrie@gmail.com>
2024-04-09 09:43:45 +1200

    Change colors

--- 0-escape-codes.ps1
+++ 0-escape-codes.ps1
@@ -16,40 +16,40 @@ try {
         [Console]::SetCursorPosition(0, 0)
 
         Write-Host ""
-        Write-SpectreRule "PowerShell terminal escape code examples" -Color "Purple_2"
+        Write-SpectreRule "PowerShell terminal escape code examples" -Color "Magenta2_1"
         Write-Host ""
 
-        $magentaForeground = $PSStyle.Foreground.FromRgb(175, 0, 255)
+        $magentaForeground = $PSStyle.Foreground.FromRgb(255, 0, 215)
         $greySlash = "[Grey30]/[/]"
 
         # Yeah this is hard to read. It's a mixture of spectre console and terminal escape codes because I wanted to put them in a table
         @(
             @{
-                Code = "[Purple_2]``a[/]"
+                Code = "[Magenta2_1]``a[/]"
                 Description = "alert"
             },
             @{
-                Code = "[Purple_2]``e[[4m[/]"
+                Code = "[Magenta2_1]``e[[4m[/]"
                 Description = "underline text"
             },
             @{
-                Code = "[Purple_2]``e[[0m[/]"
+                Code = "[Magenta2_1]``e[[0m[/]"
                 Description = "reset text"
             },
             @{
-                Code = "[Purple_2]``e[[38;2;255;0;0m[/]"
+                Code = "[Magenta2_1]``e[[38;2;255;0;0m[/]"
                 Description = "foreground rgb text"
             },
             @{
-                Code = "[Purple_2]``e[[48;2;0;155;0m[/]"
+                Code = "[Magenta2_1]``e[[48;2;0;155;0m[/]"
                 Description = "background rgb text"
             },
             @{
-                Code = "[Purple_2]``e[[1A[/] $greySlash [Purple_2]``e[[1B[/] $greySlash [Purple_2]``e[[1C[/] $greySlash [Purple_2]``e[[1D[/]"
+                Code = "[Magenta2_1]``e[[1A[/] $greySlash [Magenta2_1]``e[[1B[/] $greySlash [Magenta2_1]``e[[1C[/] $greySlash [Magenta2_1]``e[[1D[/]"
                 Description = "move cursor 1 up / down / right / left"
             },
             @{
-                Code = "[Purple_2]``r[/]"
+                Code = "[Magenta2_1]``r[/]"
                 Description = "move cursor to start of line"
             }
         ) | Format-SpectreTable -Property Code, Description -Width ([Console]::WindowWidth) -Border Rounded -Color Grey30 -HeaderColor Grey30 -AllowMarkup
@@ -63,9 +63,9 @@ try {
         Write-Host "`e[?25l`e[0J" -NoNewline
 
         Invoke-Expression "`$textInterpreted = `"$text`""
-        Write-SpectreHost "`n[Purple_2]   Plain text: [/]" -NoNewline
+        Write-SpectreHost "`n[Magenta2_1]   Plain text: [/]" -NoNewline
         Write-Host $text
-        Write-SpectreHost "[Purple_2]Terminal text: [/]" -NoNewline
+        Write-SpectreHost "[Magenta2_1]Terminal text: [/]" -NoNewline
         Write-Host $textInterpreted
         Write-Host $PSStyle.Reset -NoNewline
 
